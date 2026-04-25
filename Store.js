8a1ae9327106477a57b9b1c7d2d51988f575f443
yassine2etiizh-cloud <yassine2etiizh@gmail.com>
2026-04-25 22:56:40 +0200

    Create Store.js

---
+++ Store.js
@@ -0,0 +1,16 @@
+let products = [
+{
+id: 1,
+name: "Nike Shoes",
+price: "120€",
+cat: "shoes",
+media: ["https://example.com/shoes.jpg"]
+},
+{
+id: 2,
+name: "T-Shirt",
+price: "50€",
+cat: "clothes",
+media: ["https://example.com/shirt.jpg"]
+}
+];
