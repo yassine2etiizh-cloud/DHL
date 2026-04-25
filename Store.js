080bbaa18ebd142c0bbbdb10af6a89011df1b41f
yassine2etiizh-cloud <yassine2etiizh@gmail.com>
2026-04-25 22:59:13 +0200

    Update Store.js

--- Store.js
+++ Store.js
@@ -11,6 +11,6 @@ id: 2,
 name: "T-Shirt",
 price: "50€",
 cat: "clothes",
-media: ["https://example.com/shirt.jpg"]
+media: ["https://res.cloudinary.com/doqmnggtl/image/upload/f_auto,q_auto/IMG_1782_rz5sa5"]
 }
 ];
