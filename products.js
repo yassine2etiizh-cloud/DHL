2d5ce195045f762beeb38c346430cc67251d5e91
yassine2etiizh-cloud <yassine2etiizh@gmail.com>
2026-04-25 22:40:07 +0200

    Create products.js

---
+++ products.js
@@ -0,0 +1,16 @@
+let products = [
+  {
+    id: 1,
+    name: "Nike Air Force",
+    price: "120€",
+    cat: "shoes",
+    media: ["https://i.imgur.com/xxxx1.jpg"]
+  },
+  {
+    id: 2,
+    name: "Adidas Hoodie",
+    price: "80€",
+    cat: "clothes",
+    media: ["https://i.imgur.com/xxxx2.jpg"]
+  }
+];
